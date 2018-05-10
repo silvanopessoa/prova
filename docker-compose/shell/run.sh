bd6715977fff6e7b9cf43921d423478fa1e8723f
nogard <silvanopessoa@gmail.com>
2018-05-10 20:57:59 -0300

    Config ports

--- docker-compose/shell/run.sh
+++ docker-compose/shell/run.sh
@@ -1,2 +1,3 @@
+ cp ../spring-boot-sample-hateoas-2.0.1.RELEASE.jar .
 docker-compose up -d
-docker-compose exec db psql -U postgres -f /scripts/check.sql
+docker-compose exec db psql -U postgres -f /scripts/check.sqlrevresl
