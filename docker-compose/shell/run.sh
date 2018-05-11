dfd17e2fc87a0a7b5e83355b46ff9e08ae8a1ca7
nogard <silvanopessoa@gmail.com>
2018-05-10 21:01:50 -0300

    pasta target

--- docker-compose/shell/run.sh
+++ docker-compose/shell/run.sh
@@ -1,3 +1,3 @@
- cp ../spring-boot-sample-hateoas-2.0.1.RELEASE.jar .
+ cp ../target/spring-boot-sample-hateoas-2.0.1.RELEASE.jar .
 docker-compose up -d
 docker-compose exec db psql -U postgres -f /scripts/check.sqlrevresl
