7389982895bc731a044bb324fd9d3f6c8ee6802a
nogard <silvanopessoa@gmail.com>
2018-05-12 00:36:08 -0300

    log

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -4,4 +4,7 @@
 docker build . -t docker-compose_rest
 docker stack deploy --compose-file docker-compose.yml docker-compose_rest
 docker service scale docker-compose_rest_rest=2
+echo "Liberando porta 8090..."
 docker service update --publish-add published=8090,target=8090 docker-compose_rest_rest
+echo "Listando os serviços..."
+docker service ls
