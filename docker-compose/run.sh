968d455360cafe061b407981e7d6b379ac12a02f
nogard <silvanopessoa@gmail.com>
2018-05-12 00:24:24 -0300

    build

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -1,6 +1,7 @@
  cp ../target/spring-boot-sample-hateoas-2.0.1.RELEASE.jar .
 #docker-compose up -d
 #docker-compose exec db psql -U postgres -f /scripts/check.sql
+docker build .
 docker stack deploy --compose-file docker-compose.yml docker-compose_rest
 docker service scale docker-compose_rest_rest=2
 docker service update --publish-add published=8090,target=8090 docker-compose_rest_rest
