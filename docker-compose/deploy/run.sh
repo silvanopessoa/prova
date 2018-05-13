eca53739b852123dc4e087e20eaf5e7310271f19
nogard <silvanopessoa@gmail.com>
2018-05-13 03:05:58 -0300

    deploy

--- docker-compose/deploy/run.sh
+++ docker-compose/deploy/run.sh
@@ -1,5 +1,5 @@
-mvn install -U -f ../
-cp ../target/spring-boot-sample-hateoas-2.0.1.RELEASE.jar .
+mvn install -U -f ../../
+cp ../../target/spring-boot-sample-hateoas-2.0.1.RELEASE.jar .
 docker build . -t docker-compose_rest
 docker stack deploy --compose-file docker-compose.yml docker-compose_rest
 echo "Listando os serviços..."
