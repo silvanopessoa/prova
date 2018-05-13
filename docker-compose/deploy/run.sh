2613a69826a7561d61835995c4f7a26f31a98e5c
nogard <silvanopessoa@gmail.com>
2018-05-13 03:18:32 -0300

    Version

--- docker-compose/deploy/run.sh
+++ docker-compose/deploy/run.sh
@@ -2,5 +2,6 @@ mvn install -U -f ../../
 cp ../../target/spring-boot-sample-hateoas-2.0.1.RELEASE.jar .
 docker build . -t docker-compose_rest
 docker stack deploy --compose-file docker-compose.yml docker-compose_rest
+docker service scale docker-compose_rest_rest=2
 echo "Listando os serviços..."
 docker service ls
--- src/main/java/sample/hateoas/web/SwaggerConfiguration.java
+++ src/main/java/sample/hateoas/web/SwaggerConfiguration.java
@@ -1,10 +1,13 @@
 package sample.hateoas.web;
 
+import java.util.UUID;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +21,10 @@ public class SwaggerConfiguration {
           .select()
           .apis(RequestHandlerSelectors.basePackage(getClass().getPackage().getName()))
           .paths(PathSelectors.any())
-          .build();                                           
+          .build().apiInfo(apiInfo());                                           
     }
+    
+    private ApiInfo apiInfo() {
+    	return new ApiInfo("Prova","tarefa3",UUID.randomUUID().toString(),"","","","");
+   }
 }
