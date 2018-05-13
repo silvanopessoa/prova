687285201e2fa2199aca162334f3881d07773246
nogard <silvanopessoa@gmail.com>
2018-05-13 01:15:52 -0300

    bug

--- docker-compose/config.sh
+++ docker-compose/config.sh
@@ -6,7 +6,7 @@ sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-c
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 docker swarm init
-# https://askubuntu.com/questions/971059/17-10-openjdk-and-oracle-jdk-8-and-9-the-trustanchors-parameter-must-be-non-empt?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+# BUG - https://askubuntu.com/questions/971059/17-10-openjdk-and-oracle-jdk-8-and-9-the-trustanchors-parameter-must-be-non-empt?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 yes | sudo apt purge openjdk-11-jdk openjdk-8-jdk java-common
 yes | sudo apt install openjdk-8-jdk
 yes | sudo apt install maven
