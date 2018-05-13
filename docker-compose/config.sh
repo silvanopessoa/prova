0e753b8eadbeab4cfd99c3a99ede6cd45330621c
nogard <silvanopessoa@gmail.com>
2018-05-13 00:20:54 -0300

    bug ubuntu

--- docker-compose/config.sh
+++ docker-compose/config.sh
@@ -1,4 +1,7 @@
 sudo apt update
+# https://askubuntu.com/questions/971059/17-10-openjdk-and-oracle-jdk-8-and-9-the-trustanchors-parameter-must-be-non-empt?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+yes | sudo apt purge openjdk-11-jdk openjdk-8-jdk java-common
+sudo apt install openjdk-8-jdk
 yes | sudo apt install maven
 yes | sudo apt install docker.io
 yes | sudo apt-get install curl
