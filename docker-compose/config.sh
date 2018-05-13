c7c6ca011abbbbb3c438511f8ea31b65cb2c9dd5
nogard <silvanopessoa@gmail.com>
2018-05-12 23:02:57 -0300

    swarm

--- docker-compose/config.sh
+++ docker-compose/config.sh
@@ -1,7 +1,8 @@
 sudo apt update
 yes | sudo apt install maven
-yes | sudo apt install dockeio.io
+yes | sudo apt install docker.io
 yes | sudo apt-get install curl
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+docker swarm init
