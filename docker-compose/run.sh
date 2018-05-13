9374bddd338a6aabafbd1370e4e7a62336ab1709
nogard <silvanopessoa@gmail.com>
2018-05-12 21:31:14 -0300

    config

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -1,10 +1,9 @@
  cp ../target/spring-boot-sample-hateoas-2.0.1.RELEASE.jar .
-#docker-compose up -d
-#docker-compose exec db psql -U postgres -f /scripts/check.sql
 docker build . -t docker-compose_rest
 docker stack deploy --compose-file docker-compose.yml docker-compose_rest
-docker service scale docker-compose_rest_rest=2
 echo "Liberando porta 8090..."
 docker service update --publish-add published=8090,target=8090 docker-compose_rest_rest
+echo "Scale service API REST ..."
+docker service scale docker-compose_rest_rest=2
 echo "Listando os serviços..."
 docker service ls
